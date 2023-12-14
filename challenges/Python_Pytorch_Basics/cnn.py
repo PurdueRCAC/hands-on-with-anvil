1bf3dc3b84caecd06a5a6bae4381dfde4cbb6906
Lev Gorenstein <lev@purdue.edu>
2023-12-14 00:05:49 -0500

    Only do torch.cuda.synchronize() if actual GPUs are available

--- challenges/Python_Pytorch_Basics/cnn.py
+++ challenges/Python_Pytorch_Basics/cnn.py
@@ -155,7 +155,7 @@ model = ConvNet().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-torch.cuda.synchronize()
+if torch.cuda.is_available(): torch.cuda.synchronize()
 t1=tp.time()
 
 # Training loop
@@ -179,7 +179,7 @@ for epoch in range(num_epochs):
         if (i+1) % 500 == 0:
             print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
-torch.cuda.synchronize()
+if torch.cuda.is_available(): torch.cuda.synchronize()
 t2=tp.time()
 print('Finished Training')
 
